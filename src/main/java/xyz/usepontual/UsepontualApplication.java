108af4b1d2d51d2b4f82303a27159273d5ac36d7
João Victor <joaovictor.gizidoro@gmail.com>
2026-04-07 20:39:03 +0000

    Initial commit

---
+++ src/main/java/xyz/usepontual/UsepontualApplication.java
@@ -0,0 +1,13 @@
+package xyz.usepontual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsepontualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsepontualApplication.class, args);
+	}
+
+}
